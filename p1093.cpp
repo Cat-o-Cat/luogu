2559703ab0db2e9fdecd3fd9c812521a651dfeda
cat <236565399+Cat-o-Cat@users.noreply.github.com>
2026-01-27 16:45:27 +0800

    commit  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Changes to be committed:
    	modified:   p1093.cpp
    	new file:   p1093.md

--- p1093.cpp
+++ p1093.cpp
@@ -6,6 +6,15 @@ struct stu{
     int total;
 };
 const int MAXN=1e5+5;
+bool cmp(stu a,stu b){
+    if(a.total==b.total){
+        if(a.chinese==b.chinese){
+            return a.id<b.id;
+        }
+        return a.chinese>b.chinese;
+    }
+    return a.total>b.total;//想让大的在前return a>b;想让小的在前return a<b;
+}
 stu s[MAXN];
 int main(){
     int n;
@@ -15,5 +24,8 @@ int main(){
         s[i].total=s[i].chinese+s[i].math+s[i].english;
         s[i].id=i;
     }
-    sort(s+1,s+1+n);//需要添加一个自定义排序
+    sort(s+1,s+1+n,cmp);//需要添加一个自定义排序compare,没有定义排序规则编译器不知道如何比较这两个对象
+    for(int i=1;i<=5;i++){
+        cout<<s[i].id<<' '<<s[i].total<<endl;
+    }
 }
